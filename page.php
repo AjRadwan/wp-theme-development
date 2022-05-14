8b3588012c46d5cf9936aab61dfda9abf68106dd
Anowar <1hafizajr@gmail.com>
2022-05-14 12:25:36 +0600

    post & comment  area design

--- page.php
+++ page.php
@@ -6,12 +6,10 @@ get_header();
     <div class="container">
       <div class="row">
         <div class="col-md-9">
-     
-        <?php get_template_part('template_part/blog_setup');?>
-
-     </div>
+        <?php get_template_part('template_part/post_setup'); ?>
+        </div>
         <div class="col-md-3">
-           <?php get_sidebar();?>
+        <?php get_sidebar()?>
         </div>
       </div>
     </div>
@@ -25,7 +23,4 @@ get_footer();
 
 
 
-
-<?php wp_footer();?>
-</body>
-</html>
+ 
--- single.php
+++ single.php
@@ -6,12 +6,13 @@ get_header();
     <div class="container">
       <div class="row">
         <div class="col-md-9">
-     
-        <?php get_template_part('template_part/blog_setup');?>
-
-     </div>
+        <?php get_template_part('template_part/post_setup'); ?>
+        <div id="comment_area">
+            <?php comments_template();?>
+        </div>
+        </div>
         <div class="col-md-3">
-           <?php get_sidebar();?>
+        <?php get_sidebar()?>
         </div>
       </div>
     </div>
@@ -22,10 +23,3 @@ get_header();
 get_footer();
 ?>
     
-
-
-
-
-<?php wp_footer();?>
-</body>
-</html>
