9dd7ecee3fef8c040bf027b3b4b6825320431c6e
Anowar <1hafizajr@gmail.com>
2022-05-14 12:52:33 +0600

    search option

---
+++ search.php
@@ -0,0 +1,31 @@
+<?php
+get_header();
+?>
+  <section id="body_area">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-9">
+       <div id="search_title">
+        <h1 class="title">
+            <?php printf(__("Search Result for : %s", 'procoder'), get_search_query()); ?>
+        </h1>
+       </div>
+        <?php get_template_part('template_part/blog_setup');?>
+
+     </div>
+        <div class="col-md-3">
+           <?php get_sidebar();?>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+<?php
+get_footer();
+?>
+    
+
+<?php wp_footer();?>
+</body>
+</html>
